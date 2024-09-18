51c132451b01ffcd91d43569f7cff51f95217ec6
Rabongbong <didgmltn7769@naver.com>
2024-09-18 21:07:42 +0900

    [ADD] constant(enum, db info)

--- common/constant.py
+++ common/constant.py
@@ -1,3 +1,11 @@
-cgv_url = 'http://www.cgv.co.kr/search/?query=%uC2DC%uC0AC%uD68C'
+# 영화 시사회 정보 조회하는 URL
+cgv_url = 'http://www.cgv.co.kr/search/?query=%uC778%uC0AC'
 mega_box_url = 'https://megabox.co.kr/event/curtaincall'
 lotte_cinema_url = 'https://event.lottecinema.co.kr/NLCHS/Event/DetailList?code=40'
+
+# 데이터베이스 정보
+HOST = '127.0.0.1'
+PORT = 3306
+USER = 'root'
+PASSWORD = 'password'
+DATABASE = 'movie_preview'
