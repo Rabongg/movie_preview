49734cf4071d1609734e943780f28a05971f1e84
Rabongbong <didgmltn7769@naver.com>
2024-09-18 21:01:29 +0900

    [ADD] table DDL query

---
+++ db/init.sql
@@ -0,0 +1,9 @@
+# 필요한 테이블 정보
+CREATE TABLE movie_preview_info (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(80) NOT NULL UNIQUE,
+    period VARCHAR(60),
+    theater ENUM('cgv', 'lotte', 'megabox') NOT NULL,
+    created_dt DATETIME,
+    is_send TINYINT(1) DEFAULT 0
+);
