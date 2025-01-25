a4788695de09dc7ae8a3c18d3c08d1a618edd08b
Rabongbong <didgmltn7769@naver.com>
2025-01-25 16:03:41 +0900

    [UPDATE] indent 수정

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,6 @@
 def data_list_to_set(data_list: list[dict]) -> set:
-  data_set = set()
-  for data in data_list:
-    data_set.add(data['title'])
-  
-  return data_set
+    data_set = set()
+    for data in data_list:
+        data_set.add(data['title'])
+
+    return data_set
