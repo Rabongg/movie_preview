1663d64bfe6e64ead386ab506e03cf74c181cee5
Rabongbong <didgmltn7769@naver.com>
2025-01-31 18:26:52 +0900

    [v1] [ADD] add v1 first setting

---
+++ v1/src/main/java/com/example/movie_preview_v2/MoviePreviewV2Application.java
@@ -0,0 +1,13 @@
+package com.example.movie_preview_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviePreviewV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviePreviewV2Application.class, args);
+    }
+
+}
