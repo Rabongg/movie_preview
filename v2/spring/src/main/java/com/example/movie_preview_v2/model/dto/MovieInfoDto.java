407f9db3061f026b32e10dbf90395d4d3b19704b
Rabongbong <didgmltn7769@naver.com>
2025-02-09 22:01:11 +0900

    [v2] [spring] [UPDATE] DTO equals 및 생성자 추가

--- v2/spring/src/main/java/com/example/movie_preview_v2/model/dto/MovieInfoDto.java
+++ v2/spring/src/main/java/com/example/movie_preview_v2/model/dto/MovieInfoDto.java
@@ -1,13 +1,14 @@
 package com.example.movie_preview_v2.model.dto;
 
+import com.example.movie_preview_v2.model.entity.MovieInfo;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.util.Objects;
+
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 @ToString
 public class MovieInfoDto {
@@ -19,4 +20,22 @@ public class MovieInfoDto {
 
     private String theater;
 
+    public MovieInfoDto(MovieInfo movieInfo) {
+        this.title = movieInfo.getTitle();
+        this.date = movieInfo.getPeriod();
+        this.theater = String.valueOf(movieInfo.getTheater());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MovieInfoDto that = (MovieInfoDto) o;
+        return getTitle().equals(that.getTitle()) && getDate().equals(that.getDate()) && getTheater().equals(that.getTheater());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle(), getDate(), getTheater());
+    }
 }
