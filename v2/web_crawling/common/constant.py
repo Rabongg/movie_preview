524a8cd2a6473171e3e61edfc3502db3c44788c3
Rabongbong <didgmltn7769@naver.com>
2025-02-15 13:33:37 +0900

    [v2] [web_crawl] lotte_cinema url 수정

--- v2/web_crawling/common/constant.py
+++ v2/web_crawling/common/constant.py
@@ -1,7 +1,7 @@
 # 영화 시사회 정보 조회하는 URL
 CGV_URL = 'http://www.cgv.co.kr/search/?query=%uC778%uC0AC'
 MEGA_BOX_URL = 'https://megabox.co.kr/event/curtaincall'
-LOTTE_CINEMA_URL = 'https://event.lottecinema.co.kr/NLCHS/Event/DetailList?code=40'
+LOTTE_CINEMA_URL = 'https://www.lottecinema.co.kr/NLCHS/Event/DetailList?code=40'
 
 CGV_CLASS_TAG='searchingEventResult_list'
 MEGA_BOX_CLASS_TAG='event-list'
