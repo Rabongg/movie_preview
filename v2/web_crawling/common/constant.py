5f7bfa5bf657b9cab4c22bd3b136eb890a147071
Rabongbong <didgmltn7769@naver.com>
2025-03-05 20:20:21 +0900

    [v2] [web_crawl] [UPDATE] CGV url 수정 및 크롤링 로직 수정

--- v2/web_crawling/common/constant.py
+++ v2/web_crawling/common/constant.py
@@ -1,8 +1,8 @@
 # 영화 시사회 정보 조회하는 URL
-CGV_URL = 'http://www.cgv.co.kr/search/?query=%uC778%uC0AC'
+CGV_URL = 'http://www.cgv.co.kr/culture-event/event/defaultNew.aspx?mCode=004'
 MEGA_BOX_URL = 'https://megabox.co.kr/event/curtaincall'
 LOTTE_CINEMA_URL = 'https://www.lottecinema.co.kr/NLCHS/Event/DetailList?code=40'
 
-CGV_CLASS_TAG='searchingEventResult_list'
+CGV_CLASS_TAG='sect-evt-item-list'
 MEGA_BOX_CLASS_TAG='event-list'
 LOTTE_CLASS_TAG='img_lst_wrap'
--- v2/web_crawling/logic/cgv.py
+++ v2/web_crawling/logic/cgv.py
@@ -4,8 +4,9 @@ from logic.movie import Movie
 
 class CGV(Movie):
     def get_movie_title_and_date(self, movie_info) -> tuple[str, str]:
-        movie_title = movie_info.find_element(By.CLASS_NAME, 'searchingEventName').text
-        movie_date = movie_info.find_element(By.TAG_NAME, 'span').text
+        movie_title = movie_info.find_element(By.CLASS_NAME, 'txt1').text
+        movie_date_info = movie_info.find_element(By.CLASS_NAME, 'txt2').text
+        movie_date = movie_date_info.split()[0]
         logging.info(f"Movie Title: {movie_title}")
         logging.info(f"Movie Date: {movie_date}")
         logging.info('-' * 40)
