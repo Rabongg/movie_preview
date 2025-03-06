4ac6baec2cde70c766ae9a2729c868d9b17d6c91
Rabongbong <didgmltn7769@naver.com>
2025-03-06 23:43:19 +0900

    [v2] [web_crawl] [UPDATE] CGV URL 수정

--- v2/spring/src/main/java/com/example/movie_preview_v2/common/TheaterType.java
+++ v2/spring/src/main/java/com/example/movie_preview_v2/common/TheaterType.java
@@ -1,7 +1,7 @@
 package com.example.movie_preview_v2.common;
 
 public enum TheaterType {
-    CGV("http://www.cgv.co.kr/search/?query=%uC778%uC0AC"),
+    CGV("http://www.cgv.co.kr/culture-event/event/defaultNew.aspx?mCode=004"),
     LOTTE("https://www.lottecinema.co.kr/NLCHS/Event/DetailList?code=40"),
     MEGABOX("https://megabox.co.kr/event/curtaincall");
 
